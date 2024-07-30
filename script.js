28d4ab113f410343e4bd59ae18759df6441f6667
jsimpsonux <jsimpsonux@gmail.com>
2024-07-30 02:25:09 +0100

    Added Javascript file and created function to display projects plus some other additions

--- script.js
+++ script.js
@@ -2,10 +2,64 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function openTab(tabname) {
-    for(tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
+
+const data = [
+    {
+      title: "My Relaxing Planner",
+      image: "/images/relax.png",
+      description:
+        "A friendly planner that provides the user with a place to plan out their day, with helpful features for the user.",
+      github: "https://github.com/jsimpsonux/my-relaxing-planner",
+      link: "https://jsimpsonux.github.io/my-relaxing-planner/",
+    },
+    {
+      title: "Ingredient Alchemy",
+      image: "/images/ingredient.png",
+      description:
+        "Ingredient Alchemy allows users to be as picky as they want and still find a recipe fit for them.",
+      github: "https://github.com/jsimpsonux/Ingredient-Alchemy",
+      link: "https://jsimpsonux.github.io/Ingredient-Alchemy/",
+    },
+    {
+      title: "Crypto-Mock",
+      image: "/images/crypto.png",
+      description:
+        "A crypto website that simulates real trading with the market, and provides users with a friendly inuitive design.",
+      github: "https://github.com/jsimpsonux/Crypto-Mock",
+      link: "https://resplendent-gumption-b3f3fe.netlify.app/",
+    },
+];
+
+function Projects(data) {
+
+  let projectList = document.getElementById("project-list");
+
+  projectList.innerHTML = '';
+
+  data.forEach(data =>{
+    const projectDiv = document.createElement('div');
+
+    projectDiv.innerHTML = `
+    <h1>${data.title}</h1>
+    <img src="${data.image}">
+    <p>${data.description}</p>
+    <p><a href="${data.github}">Github Repository</p>
+    <p><a href="${data.link}">Link</p>
+    `;
+
+    projectList.appendChild(projectDiv);
+  });
+    
+
+}
+
+Projects(data);
